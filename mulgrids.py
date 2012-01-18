3ab9ed94c30bd75702ece542ee1f39c3f159a2f8
Adrian Croucher <a.croucher@auckland.ac.nz>
2012-01-18 14:15:12 +1300

    mulgrids.py: added side_lengths property to column class and used it in get_side_ratio() method

--- mulgrids.py
+++ mulgrids.py
@@ -208,10 +208,15 @@ class column(object):
         return max(angles)/min(angles)
     angle_ratio=property(get_angle_ratio)
 
+    def get_side_lengths(self):
+        "Returns list of side lengths for the column"
+        return [norm(self.node[i].pos-self.node[i-1].pos) for i in xrange(self.num_nodes)]
+    side_lengths=property(get_side_lengths)
+        
     def get_side_ratio(self):
         """Returns the side ratio for the column, defined as the ratio of the largest side length to 
         the smallest side length (a generalisation of the aspect ratio for quadrilateral columns)."""
-        l=[norm(self.node[i].pos-self.node[i-1].pos) for i in xrange(self.num_nodes)]
+        l=self.side_lengths
         return max(l)/min(l)
     side_ratio=property(get_side_ratio)
 
