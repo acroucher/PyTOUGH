4053696fb2edc05a444e4ff3623761fd4e03cbde
Adrian Croucher <a.croucher@auckland.ac.nz>
2012-05-28 15:50:05 +1200

    mulgrids.py: changed handling of start and end points in column_track(), for added robustness; changed unpacking of start and end points of track segments in slice_plot(), to allow segments with only one point; fixed default value of title in slice_plot() (was '', now None), so titles now work properly

--- mulgrids.py
+++ mulgrids.py
@@ -1382,16 +1382,19 @@ class mulgrid(object):
         """Returns a list of tuples of (column,entrypoint,exitpoint) representing the horizontal track traversed by the
         specified line through the grid.  Line is a tuple of two 2D arrays.  The resulting list is ordered by distance
         from the start of the line."""
+        def centre(crossings): return sum(crossings)/len(crossings)
         track=[]
         for col in self.columnlist:
             polygon=col.polygon
             crossings=line_polygon_intersections(polygon,line)
-            if crossings: track.append(tuple([col]+crossings))
-        sortindex=np.argsort([norm(t[1]-line[0]) for t in track])
+            if crossings:
+                if len(crossings)<2:
+                    # add start or end points if they are inside the grid:
+                    if col.contains_point(line[0]): crossings.insert(0,line[0])
+                    elif col.contains_point(line[1]): crossings.append(line[1])
+                track.append(tuple([col]+crossings))
+        sortindex=np.argsort([norm(centre(t[1])-line[0]) for t in track])
         track=[track[i] for i in sortindex]
-        # add start and end points if they are inside the grid:
-        if len(track[0])<3: track[0]=(track[0][0],line[0],track[0][1])
-        if len(track[-1])<3: track[-1]=(track[-1][0],track[-1][1],line[1])
         return track
 
     def layer_plot(self,layer=0,variable=None,variable_name=None,unit=None,column_names=None,node_names=None,column_centres=None,nodes=None,colourmap=None,linewidth=0.2,linecolour='black',aspect='equal',plt=None,subplot=111,title=None,xlabel='x (m)',ylabel='y (m)',contours=False,contour_label_format='%3.0f',contour_grid_divisions=(100,100),connections=None,colourbar_limits=None,plot_limits=None):
@@ -1497,7 +1500,7 @@ class mulgrid(object):
             plt.title(title)
             if loneplot: plt.show()
 
-    def slice_plot(self,line=None,variable=None,variable_name=None,unit=None,block_names=None,colourmap=None,linewidth=0.2,linecolour='black',aspect='auto',plt=None,subplot=111,title='',xlabel=None,ylabel='elevation (m)',contours=False,contour_label_format='%3.0f',contour_grid_divisions=(100,100),colourbar_limits=None,plot_limits=None):
+    def slice_plot(self,line=None,variable=None,variable_name=None,unit=None,block_names=None,colourmap=None,linewidth=0.2,linecolour='black',aspect='auto',plt=None,subplot=111,title=None,xlabel=None,ylabel='elevation (m)',contours=False,contour_label_format='%3.0f',contour_grid_divisions=(100,100),colourbar_limits=None,plot_limits=None):
         """Produces a vertical slice plot of a Mulgraph grid, shaded by the specified variable (an array of values for each block).
        A unit string can be specified for annotation.  Block names can be optionally superimposed, and the colour 
        map, linewidth, aspect ratio, colour-bar limits and plot limits specified.
@@ -1564,7 +1567,11 @@ class mulgrid(object):
             verts,vals=[],[]
             if not isinstance(contours,bool): contours=list(contours)
             if contours<>False: xc,yc=[],[]
-            for col,inpoint,outpoint in track:
+            for trackitem in track:
+                col,points=trackitem[0],trackitem[1:]
+                inpoint=points[0]
+                if len(points)>1: outpoint=points[1]
+                else: outpoint=inpoint
                 din,dout=norm(inpoint-l[0]),norm(outpoint-l[0])
                 for lay in self.layerlist[1:]:
                     if col.surface>lay.bottom:
