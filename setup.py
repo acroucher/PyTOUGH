7803ee065fcc9c01bf2440f5dbb9f85f0179d415
Adrian Croucher <a.croucher@auckland.ac.nz>
2012-09-17 10:27:11 +1200

    Added setup.py, for source distributions

---
+++ setup.py
@@ -0,0 +1,12 @@
+# PyTOUGH setup script
+from distutils.core import setup
+
+setup(name='PyTOUGH',
+      version='1.3',
+      description='Python scripting library for TOUGH2 simulation',
+      author='Adrian Croucher',
+      author_email='a.croucher@auckland.ac.nz',
+      url='https://github.com/acroucher/PyTOUGH',
+      license='LGPL',
+      py_modules=['geometry','IAPWS97','mulgrids','t2data','t2grids','t2incons','t2listing','t2thermo'],
+      )
