5278b13a6095c7599b4624bf83cbc9f9211e3b07
Adrian Croucher <a.croucher@auckland.ac.nz>
2012-10-08 14:15:16 +1300

    Version 1.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='PyTOUGH',
-      version='1.3.2',
+      version='1.3.3',
       description='Python scripting library for TOUGH2 simulation',
       author='Adrian Croucher',
       author_email='a.croucher@auckland.ac.nz',
