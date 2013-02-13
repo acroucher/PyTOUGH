c2c957038991b490bcfba8fa9e3118171f6bda5a
Adrian Croucher <a.croucher@auckland.ac.nz>
2013-02-13 15:12:14 +1300

    Added fixed_format_file to list of modules in setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='PyTOUGH',
       author_email='a.croucher@auckland.ac.nz',
       url='https://github.com/acroucher/PyTOUGH',
       license='LGPL',
-      py_modules=['geometry','IAPWS97','mulgrids','t2data','t2grids','t2incons','t2listing','t2thermo'],
+      py_modules=['fixed_format_file','geometry','IAPWS97','mulgrids','t2data','t2grids','t2incons','t2listing','t2thermo'],
       )
