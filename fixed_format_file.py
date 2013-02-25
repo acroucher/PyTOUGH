0d15e6b6f39de8fbfe6d2525af26a2a4b07fe265
Adrian Croucher <a.croucher@auckland.ac.nz>
2013-02-25 13:36:31 +1300

    Added LGPL license statement to fixed_format_file.py

--- fixed_format_file.py
+++ fixed_format_file.py
@@ -1,4 +1,15 @@
-"""Reading, parsing and writing fixed format text files."""
+"""For reading, parsing and writing fixed format text files."""
+
+"""
+Copyright 2013 University of Auckland.
+
+This file is part of PyTOUGH.
+
+PyTOUGH is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+PyTOUGH is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with PyTOUGH.  If not, see <http://www.gnu.org/licenses/>."""
 
 class fixed_format_file(file):
 
