2148c3daead13f9e9dc6b6da5df4364267ba3f15
Adrian Croucher <a.croucher@auckland.ac.nz>
2024-02-12 16:57:38 +1300

    Update to pyproject.toml for packaging

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-# PyTOUGH setup script
-from distutils.core import setup
-
-setup(name = 'PyTOUGH',
-      version = '1.5.7',
-      description = 'Python scripting library for TOUGH2 simulation',
-      author = 'Adrian Croucher',
-      author_email = 'a.croucher@auckland.ac.nz',
-      url = 'https://github.com/acroucher/PyTOUGH',
-      license = 'LGPL',
-      py_modules = [
-          'fixed_format_file',
-          'geometry',
-          'IAPWS97',
-          'mulgrids',
-          't2data',
-          't2grids',
-          't2incons',
-          't2listing',
-          't2thermo'],
-      )
+from setuptools import setup
+# for backwards compatibility only
+setup()
